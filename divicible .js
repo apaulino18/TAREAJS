cfb393a11a4e4a59887a28c1b0810a0f0515a8b4
apaulino18 <103431112+apaulino18@users.noreply.github.com>
2022-04-19 20:24:23 -0400

    Update divicible .js

--- divicible .js
+++ divicible .js
@@ -1,15 +1,19 @@
 
 const prompt = require('prompt');
-prompt.start()
+prompt.start();
 
 prompt.get(['num1'], function (err, divi) {
+num1 = parseInt(divi.num1);
+num2 = 2;
+while
+(num1 % num2 !==0 && num2 < num1 / 2) {
+num2++;
 
-if (divi.num1 % 2 === 0 )
-     {
-console.log("Es divisible por 2" + divi.num1);
+}
+if (num1 % num2 !== 0 )  {
+console.log("El numero es primo");
      } 
-    else
-     {
-console.log("No es divisible por 2" + divi.num1);
+    else  {
+console.log("No es primo1");
      }
 });
